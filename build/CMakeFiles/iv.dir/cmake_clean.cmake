e5187b367eaf34da90d1b6329ccda65f88874a73
Oliver Rupprecht <oliverjrupprecht@gmail.com>
2026-03-29 12:54:32 +0100

    changed to cmake as build tool, refactored window creation so that it pumps Cocoa loop

---
+++ build/CMakeFiles/iv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iv.dir/iv.c.o"
+  "CMakeFiles/iv.dir/iv.c.o.d"
+  "iv"
+  "iv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/iv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- iv.c
+++ iv.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL.h>
 
 int main() {
 	printf("Hello world");
+
+	SDL_Window *pwindow = SDL_CreateWindow("Image Viewer",
+					SDL_WINDOWPOS_CENTERED,
+					SDL_WINDOWPOS_CENTERED,
+					900,
+					600,
+					0);
+
+	SDL_Event e;
+	int running = 1;
+
+	while (running) {
+	    while (SDL_PollEvent(&e)) {
+		if (e.type == SDL_QUIT) running = 0;
+	    }
+	    // render here
+	}
 }
+
