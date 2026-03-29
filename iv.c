f2b4af0f4dc22b0256b597f68bfce84e32e0f4f2
Oliver Rupprecht <oliverjrupprecht@gmail.com>
2026-03-29 16:44:13 +0100

    stopped tracking build files

--- iv.c
+++ iv.c
@@ -18,18 +18,19 @@ int main() {
 	SDL_Surface *psurface = SDL_GetWindowSurface(pwindow); // from window struct returns p to surface area 
 
 	Uint8 r,g,b;
-	r = 0x00;
+	r = 0xFF;
 	g = b = 0;
 
 	Uint32 colour = SDL_MapRGB(psurface->format,r,g,b); // the -> syntax is sugar for (*ptr).field
 	
-	SDL_FillRect(psurface, NULL, colour);
+	int x = 50;
+	int y = 50;
 
+	SDL_Rect pixel = (SDL_Rect){x,y,1,1}; // compound literal and casting it to rect type
+	SDL_FillRect(psurface, &pixel, colour);
 
-	
 	SDL_Event e;
 	int running = 1;
-
 	while (running) {
 		while (SDL_PollEvent(&e)) { // each loop see if event has occured
 			if (e.type == SDL_QUIT) running = 0;
